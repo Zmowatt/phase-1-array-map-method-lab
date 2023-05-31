a0d4e660c9f6397d2216ce087a16bbc66b06ac76
Zachary Mowatt <zamowatt@gmail.com>
2023-05-31 00:10:40 -0400

    completed lab

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+const titleCased = () =>{
+  return tutorials.map((tutorial) => {
+    const words = tutorial.split(' ');
+    const upperCasedWords = words.map((word) =>{
+      const upperCaseLetter = word.charAt(0).toUpperCase();
+      const lowerCaseLetters = word.slice(1);
+      return upperCaseLetter + lowerCaseLetters;
+    });
+    return upperCasedWords.join(' ');
+  })
 }
